4beb4c61aa558082cae01497fb92c7733a64a473
Doug Wilson <doug@beelineweb.com>
2013-05-27 08:42:05 -0700

    Clicking a TextBox to give it focus now selects the contents of the textbox

---
+++ src/BeeZap/BeeZap/Infrastructure/BeeTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Beeline.BeeZap.Infrastructure
+{
+	public class BeeTextBox : TextBox
+	{
+		private bool _focused;
+
+		protected override void OnEnter(EventArgs e) {
+			base.OnEnter(e);
+			if (MouseButtons == MouseButtons.None) {
+				SelectAll();
+				_focused = true;
+			}
+		}
+
+		protected override void OnLeave(EventArgs e) {
+			base.OnLeave(e);
+			_focused = false;
+		}
+
+		protected override void OnMouseUp(MouseEventArgs mevent) {
+			base.OnMouseUp(mevent);
+			if (!_focused) {
+				if (SelectionLength == 0)
+					SelectAll();
+				_focused = true;
+			}
+		}
+	}
+}
